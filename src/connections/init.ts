c9eb04d8d160980ca9ae1b57987c343fd6e7fb39
johnlin <john830110@gmail.com>
2023-06-03 11:58:08 +0800

    feat: 修改初始化腳本

--- src/connections/init.ts
+++ src/connections/init.ts
@@ -3,6 +3,12 @@ import dotenv from "dotenv";
 import { initUsers } from "../db/initUser"
 import { initProposers } from "../db/initProposer";
 import { initProjects } from "../db/initProject";
+import { initOptions } from "../db/initOptions";
+import { initQas } from "../db/initQas";
+import { initNews } from "../db/initNews";
+import { initOrders } from "../db/initOrder";
+import { initOrderInfos } from "../db/initOrderInfo";
+import { initLikes } from "../db/initLikes";
 
 dotenv.config({ path: "./.env" });
 
@@ -19,6 +25,12 @@ mongoose.connect(DB).then(async () => {
   await initUsers();
   await initProposers();
   await initProjects();
+  await initOptions();
+  await initQas();
+  await initNews();
+  await initOrders();
+  await initOrderInfos();
+  await initLikes();
   console.log("資料庫初始化資料完成");
 }).then(() => {
   mongoose.connection.close();
