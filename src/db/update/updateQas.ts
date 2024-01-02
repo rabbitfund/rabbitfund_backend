faa440d6bba3d3b1311232b45a95eedc16d5bd19
johnlin <john830110@gmail.com>
2024-01-03 00:06:01 +0800

    refactor: 簡化寫法

--- src/db/update/updateQas.ts
+++ src/db/update/updateQas.ts
@@ -1,29 +1,23 @@
 import Qas from "../../model/qasModels";
 
+type UpdateFields = {
+  qas_create_date: Date;
+  qas_update_date?: Date;
+}
+
 const postponeQas = async (nYears: number = 1) => {
-  // Fetch all documents
   const qasDocuments = await Qas.find();
 
   for (const qasDocument of qasDocuments) {
-    let newQas;
+    const updateFields: UpdateFields = {
+      qas_create_date: new Date(qasDocument.qas_create_date.setFullYear(qasDocument.qas_create_date.getFullYear() + nYears))
+    };
+
     if (qasDocument.qas_update_date) {
-      newQas = await Qas.findByIdAndUpdate(
-        qasDocument._id,
-        {
-          qas_create_date: new Date(qasDocument.qas_create_date.setFullYear(qasDocument.qas_create_date.getFullYear() + nYears)),
-          qas_update_date: new Date(qasDocument.qas_update_date.setFullYear(qasDocument.qas_update_date.getFullYear() + nYears))
-        },
-        { new: true }
-      );
-    } else {
-      newQas = await Qas.findByIdAndUpdate(
-        qasDocument._id,
-        {
-          qas_create_date: new Date(qasDocument.qas_create_date.setFullYear(qasDocument.qas_create_date.getFullYear() + nYears))
-        },
-        { new: true }
-      );
+      updateFields.qas_update_date = new Date(qasDocument.qas_update_date.setFullYear(qasDocument.qas_update_date.getFullYear() + nYears));
     }
+
+    const newQas = await Qas.findByIdAndUpdate(qasDocument._id, updateFields, { new: true });
 
     if (!newQas) {
       console.log('更新 Qas 失敗');
